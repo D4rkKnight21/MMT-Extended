19b2c33f1937e82dc831e3ac70a387178cf06000
Yusuf Saiful Abidin <yusuff8621@gmail.com>
2021-03-28 16:46:47 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -17,7 +17,14 @@
 #DEBUG=true
 
 ##########################################################################################
-# Replace list
+#                                Tissot-Max
+#                                    v1
+#                       Reach your maximum performance
+#
+#           We are not responsible for future damage to your device
+#
+# Instalationn........
+# 
 ##########################################################################################
 
 # List all directories you want to directly replace in the system
@@ -50,18 +57,16 @@ set_permissions() {
   # For directories (includes files in them):
   # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
   
-  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
-  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
+  # set_perm_recursive $MODPATH/system/vendor/etc/thermal-engine.conf 0 0 0755 0644
 
   # For files (not in directories taken care of above)
   # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
   
-  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
-  # set_perm /data/local/tmp/file.txt 0 0 644
 }
 
 ##########################################################################################
-# MMT Extended Logic - Don't modify anything after this
+#                                    Finished 
+#                  Enjoy ur gayming , enjoy ur "setrika portable"
 ##########################################################################################
 
 SKIPUNZIP=1
